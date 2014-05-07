5ebf2b9372ed904399169f15ed7086e277655f02
Johannes Zarl <johannes.zarl@jku.at>
2014-05-07 17:57:37 +0200

    Fix whitespace and tabs.

--- ipmi/python_modules/ipmi.py
+++ ipmi/python_modules/ipmi.py
@@ -21,42 +21,42 @@ def get_metrics():
 
     if (time.time() - METRICS['time']) > METRICS_CACHE_MAX:
 
-	new_metrics = {}
-	units = {}
-
-	command = [ params['timeout_bin'] , "3", params['ipmitool_bin'] , "-H", params['ipmi_ip'] , "-U" , params['username'] , '-P', params['password'] , 'sensor']	
-
+        new_metrics = {}
+        units = {}
+    
+        command = [ params['timeout_bin'] , "3", params['ipmitool_bin'] , "-H", params['ipmi_ip'] , "-U" , params['username'] , '-P', params['password'] , 'sensor']
+    
         p = subprocess.Popen(command,
                              stdout=subprocess.PIPE).communicate()[0][:-1]
-
+    
         for i, v in enumerate(p.split("\n")):
             data = v.split("|")
             try:
                 metric_name = data[0].strip().lower().replace("+", "").replace(" ", "_")
                 value = data[1].strip()
-
+    
                 # Skip missing sensors
                 if re.search("(0x)", value ) or value == 'na':
                     continue
-
+    
                 # Extract out a float value
                 vmatch = re.search("([0-9.]+)", value)
                 if not vmatch:
                     continue
                 metric_value = float(vmatch.group(1))
-
+    
                 new_metrics[metric_name] = metric_value
                 units[metric_name] = data[2].strip().replace("degrees C", "C")
-		
+    
             except ValueError:
                 continue
             except IndexError:
                 continue
-		
-	METRICS = {
-            'time': time.time(),
-            'data': new_metrics,
-            'units': units
+
+        METRICS = {
+                'time': time.time(),
+                'data': new_metrics,
+                'units': units
         }
 
     return [METRICS]
@@ -66,13 +66,12 @@ def get_value(name):
     """Return a value for the requested metric"""
 
     try:
+        metrics = get_metrics()[0]
 
-	metrics = get_metrics()[0]
+        prefix_length = len(params['metric_prefix']) + 1
+        name = name[prefix_length:] # remove prefix from name
 
-	prefix_length = len(params['metric_prefix']) + 1
-	name = name[prefix_length:] # remove prefix from name
-
-	result = metrics['data'][name]
+        result = metrics['data'][name]
 
     except Exception:
         result = 0
@@ -100,16 +99,16 @@ def metric_init(params):
         'slope'       : 'both', # zero|positive|negative|both
         'description' : 'XXX',
         'groups'      : 'XXX',
-        }
+    }
 
     metrics = get_metrics()[0]
     
     for item in metrics['data']:
-	descriptors.append(create_desc(Desc_Skel, {
-		"name"       	: params['metric_prefix'] + "_" + item,
-		'groups'	: params['metric_prefix'],
-		'units'		: metrics['units'][item]
-		}))
+        descriptors.append(create_desc(Desc_Skel, {
+            "name"      : params['metric_prefix'] + "_" + item,
+            'groups'    : params['metric_prefix'],
+            'units'     : metrics['units'][item]
+        }))
 
 
     return descriptors
@@ -123,10 +122,10 @@ if __name__ == '__main__':
     
     params = {
         "metric_prefix" : "ipmi",
-	"ipmi_ip" : "10.1.2.3",
-	"username"  : "ADMIN",
-	"password"  : "secret"
-	}
+        "ipmi_ip" : "10.1.2.3",
+        "username"  : "ADMIN",
+        "password"  : "secret"
+    }
     descriptors = metric_init(params)
 
     while True:
